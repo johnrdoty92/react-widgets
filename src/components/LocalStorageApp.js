5430c1effb8f39bdc14dbad6917cd94f9a75ec03
John Doty <johnrdoty92@gmail.com>
2021-07-08 16:41:49 -0400

    local Storage app

---
+++ src/components/LocalStorageApp.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+
+class LocalStorageApp extends Component {
+  state = { user: "", rememberMe: false };
+
+  handleChange = (event) => {
+    const input = event.target;
+    const value = input.type === "checkbox" ? input.checked : input.value;
+
+    this.setState({ [input.name]: value });
+  };
+
+  handleFormSubmit = () => {
+    const { user, rememberMe } = this.state;
+    localStorage.setItem("rememberMe", rememberMe);
+    localStorage.setItem("user", rememberMe ? user : "");
+  };
+
+  render() {
+    const userName = localStorage.getItem("rememberMe")
+      ? localStorage.getItem("user")
+      : null;
+
+    return (
+      <form className="card p-4 shadow-sm" onSubmit={this.handleFormSubmit}>
+        <h2 className="text-center">
+          {localStorage.getItem("user")
+            ? `Hello, ${userName}!`
+            : "Please Log In"}
+        </h2>
+        <div className="container">
+          <label className="form-label col" for="user">
+            User:
+            <input
+              id="user"
+              className="form-control"
+              name="user"
+              type="text"
+              value={this.state.user}
+              onChange={this.handleChange}
+            />
+          </label>
+          <div className="form-check">
+            <input
+              id="rememberMe"
+              className="form-check-input"
+              type="checkbox"
+              checked={this.state.rememberMe}
+              name="rememberMe"
+              onChange={this.handleChange}
+            />
+            <label className="form-check-label" for="rememberMe">
+              Remember me
+            </label>
+          </div>
+        </div>
+        <button className="btn btn-info w-50 align-self-center" type="submit">
+          Sign In
+        </button>
+      </form>
+    );
+  }
+}
+
+export default LocalStorageApp;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Calculator from "./components/calculator/Calculator";
 import WeatherApp from "./components/weatherApp/WeatherApp.js";
 import TicTacToe from "./components/TicTacToe.js";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import LocalStorageApp from "./components/LocalStorageApp";
 
 //================================ WIDGETS ================================
 class Widgets extends React.Component {
@@ -13,6 +14,8 @@ class Widgets extends React.Component {
       <div id="home" className="container">
         <h1 className="text-center m-2">Weather Forecast</h1>
         <WeatherApp />
+        <h1 className="text-center m-2">Using Local Storage</h1>
+        <LocalStorageApp />
         <h1 className="text-center m-2">Calculator</h1>
         <Calculator />
         <h1 className="text-center m-2">Tic Tac Toe</h1>
